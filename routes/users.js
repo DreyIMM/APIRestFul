8ff82a6eba7407187bad28b008d79db0875a63a0
DreyIMM <andreydario26@gmail.com>
2022-02-06 15:07:30 -0300

    Listando users

--- routes/users.js
+++ routes/users.js
@@ -11,17 +11,24 @@ module.exports = app=>{
     
     app.get('/users' ,(req,res)=>{
 
-        res.statusCode = 200;
-        res.setHeader ('Content-Type', 'application/json');
-        res.json({
-            
-            users:[{
-                id: 1,
-                name:'Andrey',
-                email:'andrey.barbosa@ibm.com'
-            }]
+        db.find({}).sort({name:1}).exec((err,users)=>{
+            if(err){
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                })
+            }else{
+                res.statusCode = 200;
+                res.setHeader ('Content-Type', 'application/json');
+                res.json({
+                
+                users:users
     
         });
+            }
+        })
+
+        
     
     });
     
